add37dc2495b204306f2388cbf4f08d6fc15640a
xampv567@gmail.com <exnite110@gmail.com>
2024-07-12 22:38:43 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PresensiController;
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+//ini di akses ketika user belum login
+Route::middleware(['guest:karyawan'])->group(function() {
+    Route::get('/', function () {
+        return view('auth.login');
+    })->name('login');
+    Route::post('/proseslogin', [AuthController::class, 'proseslogin']);
+});
+
+
+//agar user tidak gampang aces dashboard
+
+Route::middleware(['auth:karyawan'])->group(function(){
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/proseslogout', [AuthController::class, 'proseslogout']);
+
+    //Presensi
+    Route::get('/presensi/create', [PresensiController::class,'create']);
+});
